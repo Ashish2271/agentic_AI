08155f449df62c0992047496cfce6737b107a012
Ashish <ashispandey138c@gmail.com>
2026-05-02 18:20:28 +0530

    added few more models

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ AVAILABLE_MODELS = {
     "gpt-oss": "openai/gpt-oss-120b",
     "ngredia":"nvidia/nemotron-3-nano-30b-a3b",
     "chinese": "tencent/hy3-preview",
-
-    
+    "kimi_2.6": "moonshotai/kimi-k2.6",
+    "kimi_2.5": "moonshotai/kimi-k2.5",
+    "ling-2.6-1t": "inclusionai/ling-2.6-1t"
 }
